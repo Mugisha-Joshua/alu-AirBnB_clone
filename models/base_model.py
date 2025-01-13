957d3c2fad5163cf23a2c45b636c5008c4ada781
TonnyleeM <muvunyilee@gmail.com>
2025-01-13 02:08:03 -0800

    complete

--- models/base_model.py
+++ models/base_model.py
@@ -10,42 +10,39 @@
 # you can use isoformat() of datetime object
 
 
+#!/usr/bin/python3
+"""Module for BaseModel class."""
 from datetime import datetime
 import uuid
-import os
-class BaseModel:
+import models
 
- def __init__(self, *args, **kwargs): 
-     if kwargs:
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != "__class__":
                     setattr(self, key, value)
-            if "id" not in kwargs:
-                self.id = str(uuid.uuid4())
-            if "created_at" not in kwargs:
-                self.created_at = datetime.now()
-            if "updated_at" not in kwargs:
-                self.updated_at = datetime.now()
-            else:
-     self.id=str(uuid.uuid4())
-     self.created_at=datetime.now()
-     self.updated_at=datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
- def __str__(self):
-    return "[{}] ({}) {};/".format(self.__class__.__name__, self.if, self.__dict__)
+    def __str__(self):
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
- def save(self):
+    def save(self):
         self.updated_at = datetime.now()
         models.storage.save()
 
-def to_dict(self):
-        inst_dict = self.__dict__.copy()
-        inst_dict["__class__"] = self.__class__.__name__
-        inst_dict["created_at"] = self.created_at.isoformat()
-        inst_dict["updated_at"] = self.updated_at.isoformat()
-
-        return inst_dict
-
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
 
