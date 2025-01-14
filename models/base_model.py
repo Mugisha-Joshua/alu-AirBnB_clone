fef73eb5ee060327d8118acb1ad50b86ec2b0dc7
TonnyleeM <muvunyilee@gmail.com>
2025-01-14 04:08:29 -0800

    complete

--- models/base_model.py
+++ models/base_model.py
@@ -11,22 +11,30 @@
 
 
 #!/usr/bin/python3
-"""Module for BaseModel class."""
-from datetime import datetime
+"""
+Module for the BaseModel class.
+"""
 import uuid
-import model:
-    def __str__(self):
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__)
+from datetime import datetime
+import models
+
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
 
     def save(self):
-        self.updated_at = datetime.now()
-        models.storage.save()
+        self.updated_at = datetime.utcnow()
 
     def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict["__class__"] = self.__class__.__name__
-        obj_dict["created_at"] = self.created_at.isoformat()
-        obj_dict["updated_at"] = self.updated_at.isoformat()
-        return obj_dict
+        inst_dict = self.__dict__.copy()
+        inst_dict["__class__"] = self.__class__.__name__
+        inst_dict["created_at"] = self.created_at.isoformat()
+        inst_dict["updated_at"] = self.updated_at.isoformat()
 
+        return inst_dict
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
