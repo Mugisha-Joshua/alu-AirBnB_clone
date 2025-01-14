9a62a6b083432f0febd3ecf9ed27835163b13af2
TonnyleeM <muvunyilee@gmail.com>
2025-01-14 03:46:29 -0800

    complete

--- models/base_model.py
+++ models/base_model.py
@@ -14,23 +14,7 @@
 """Module for BaseModel class."""
 from datetime import datetime
 import uuid
-import models
-
-
-class BaseModel:
-    def __init__(self, *args, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-                elif key != "__class__":
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
-
+import model:
     def __str__(self):
         return "[{}] ({}) {}".format(
             self.__class__.__name__, self.id, self.__dict__)
