4671120ec59c9676be3d1dd983e640dcd3026b47
TonnyleeM <160362104+TonnyleeM@users.noreply.github.com>
2025-01-17 14:12:18 +0200

    better version

--- models/base_model.py
+++ models/base_model.py
@@ -16,25 +16,33 @@ Module for the BaseModel class.
 """
 import uuid
 from datetime import datetime
-import models
 
 
 class BaseModel:
+    """Base class for all models in the HBNB clone"""
+    
     def __init__(self):
+        """Initialize a new BaseModel instance"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
 
     def save(self):
+        """Update the updated_at timestamp"""
         self.updated_at = datetime.utcnow()
 
     def to_dict(self):
+        """Convert instance to dictionary"""
         inst_dict = self.__dict__.copy()
         inst_dict["__class__"] = self.__class__.__name__
         inst_dict["created_at"] = self.created_at.isoformat()
         inst_dict["updated_at"] = self.updated_at.isoformat()
-
         return inst_dict
 
     def __str__(self):
-        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+        """Return string representation of the instance"""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, 
+            self.id, 
+            self.__dict__
+        )
