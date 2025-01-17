7b5532d2827353dff61094c1f026afe6390e4677
Mugisha-Joshua <o.mugisha2@alustudent.com>
2025-01-17 14:22:28 +0200

    Updated files

--- models/base_model.py
+++ models/base_model.py
@@ -15,26 +15,25 @@
 Module for the BaseModel class.
 """
 import uuid
-from datetime import datetime
-import models
+import datetime
+
 
 
 class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def save(self):
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        inst_dict = self.__dict__.copy()
-        inst_dict["__class__"] = self.__class__.__name__
-        inst_dict["created_at"] = self.created_at.isoformat()
-        inst_dict["updated_at"] = self.updated_at.isoformat()
-
-        return inst_dict
+        return{
+            "id": self.id,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat(),
+        }
 
     def __str__(self):
-        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
